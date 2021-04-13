ce8d41925f8738574da32d7e2fe93e4820376535
Nikolai9906 <nicohhhh1@gmail.com>
2021-04-12 21:59:39 -0500

    CircleCi working

--- src/test/java/com/masiv/CallcenterApplicationTests.java
+++ src/test/java/com/masiv/CallcenterApplicationTests.java
@@ -1,10 +1,13 @@
 package com.masiv;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+
 class CallcenterApplicationTests {
+
 
 	@Test
 	void contextLoads() {
